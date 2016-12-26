a695fe027abb6f5e6be16bb5a76e0d07b5511d31
d-smith <doug.smith.mail@gmail.com>
2016-12-26 07:44:22 -0800

    Added listener and target group

--- alb/alb.tf
+++ alb/alb.tf
@@ -33,3 +33,21 @@ resource "aws_alb" "test" {
     Name = "ECS ALB"
   }
 }
+
+resource "aws_alb_target_group" "test_tg" {
+  name     = "tf-example-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+}
+
+resource "aws_alb_listener" "test_listener" {
+   load_balancer_arn = "${aws_alb.test.arn}"
+   port = "80"
+   protocol = "HTTP"
+
+   default_action {
+     target_group_arn = "${aws_alb_target_group.test_tg.arn}"
+     type = "forward"
+   }
+}
